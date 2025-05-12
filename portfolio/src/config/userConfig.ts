59e8176276af9b3b905b81a0c93936e664e565af
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-05-12 00:31:00 -0500

    feat: Add user configuration file with personal and project details

---
+++ portfolio/src/config/userConfig.ts
@@ -0,0 +1,278 @@
+import neurospeccompanionmerged from "./projects/neurospeccompanionmerged.json";
+import scheds from "./projects/scheds.json";
+import fadedTextRestoration from "./projects/fadedtextrestoration.json";
+import clinicalmain from "./projects/clinicalmain.json";
+import seatReservation from "./projects/seatreservation.json";
+import nucpaBalloons from "./projects/nucpa-balloons.json";
+import nucpaBalloonsApi from "./projects/nucpaballoonsapi.json";
+import portfolio from "./projects/portfolio.json";
+import foodies from "./projects/foodies.json";
+export const userConfig = {
+  // Personal Information
+  name: "Cristhian Agamez",
+  role: "Software Developer - Software Architect",
+  location: "Barranquilla, Colombia",
+  email: "cristhian_agamez@outlook.com",
+  website: "cristhianac.dev",
+  roleFocus:
+    "Software architect and developer with a focus on building scalable and maintainable software solutions. I have a strong background in software development, architecture, and design patterns, and I am passionate about creating high-quality software that meets the needs of users and businesses. I am always looking for new challenges and opportunities to learn and grow as a developer.",
+  age: 21,
+
+  // Social Links
+  social: {
+    github: "https://github.com/CristhianAC",
+    linkedin: "www.linkedin.com/in/cristhianac", // Add your LinkedIn URL
+  },
+
+  // Contact Information
+  contact: {
+    email: "cristhian_agamez@outlook.com",
+    phone: "+57 3045989898", // Add your phone number
+    calendly: "", // Add your Calendly URL
+  },
+
+  // Spotify Configuration
+  spotify: {
+    playlistId: "06v44KxhE2VpqXxQWiLO78", // Your Spotify playlist ID
+    playlistName: "De Toda Vaina",
+  },
+
+  // Resume Configuration
+  resume: {
+    url: "https://drive.google.com/file/d/16l52Bxpd6xGAkFWQ0S7WFZ1OQwUox_Dy/view?usp=sharing",
+    localPath: "/CV.pdf",
+  },
+
+  // Education Background
+  education: [
+    {
+      degree: "Bachelor of Computer Science",
+
+      institution: "Universidad del Norte",
+      location: "Barranquilla, Colombia",
+      year: "2021-2025",
+      description:
+        "Relevant coursework: Data Structures, Algorithms, Database Management, Software Engineering, Biomedical Informatics, Artificial Intelligence, Machine Learning, Deep Learning, Computer Vision, Natural Language Processing, Computer Networks, Operating Systems, Computer Architecture, Computer Organization, Computer Security, Computer Graphics, Computer Systems, Computer Networks, Operating Systems, Computer Architecture, Computer Organization, Computer Security, Computer Graphics, Computer Systems",
+      images: [
+        {
+          url: "",
+          alt: "Universidad Del Norte",
+          description: "Universidad del Norte Campus",
+        },
+      ],
+    },
+  ],
+
+  courses: [
+    {
+      title: "Python & Django: REST API Blockchain",
+      description: "Build & Python Web API",
+      institution: "Udemy",
+      location: "Colombia",
+      year: "2023",
+    },
+    {
+      title: "Flutter for Beginners",
+      description: "Flutter App Development",
+      institution: "Udemy",
+      location: "Colombia",
+      year: "2023",
+    },
+    {
+      title: "Angular for Juniors: Building Real World Application",
+      description: "Learn how to build real-world applications with Angular",
+      institution: "Udemy",
+      location: "Colombia",
+      year: "2023",
+    },
+    {
+      title: "Introducción a Blender: Modelado 3D",
+      description: "Learn Blender 3D modeling and animation tools",
+      institution: "Udemy",
+      location: "Colombia",
+      year: "2023",
+    },
+    {
+      title: "Introducción a Adobe Premiere Pro",
+      description: "Learn video editing with Adobe Premiere Pro",
+      institution: "Domestika",
+      location: "Colombia",
+      year: "2023",
+    },
+    {
+      title: "Xamarin: Desarrollo de aplicaciones móviles",
+      description: "Build cross-platform mobile apps with Xamarin",
+      institution: "Udemy",
+      location: "Colombia",
+      year: "2023",
+    },
+  ],
+
+  skills: [
+    "Astro",
+    "Typescript",
+    "Angular",
+    "React",
+    "Svelte",
+    "Django",
+    ".Net",
+    "Figma",
+    "Illustrator",
+    "Photoshop",
+    "FastApi",
+    "NestJs",
+    "PostgreSQL",
+    "React Native",
+    "Docker",
+    "Kubernetes",
+    "Oracle",
+    "Azure",
+  ],
+
+  extraCurricularRoles: [
+    {
+      role: "President Of Innova",
+      institution: "Uninorte",
+      location: "Universidad del Norte",
+      year: "2023-2024",
+    },
+
+    {
+      role: "Mentor and tutor",
+      institution: "Uninorte",
+      location: "Barranquilla",
+      year: "2021-2024",
+    },
+  ],
+
+  extraCurricularActivities: [
+    {
+      title:
+        "Winner for the first debate's tournament from the Universidad Del Norte",
+      description:
+        "First place in the inaugural debate tournament at Universidad del Norte, showcasing skills in argumentation, critical thinking, and public speaking.",
+      institution: "Universidad del Norte",
+      location: "Universidad del Norte, Barranquilla, Colombia",
+      year: "2025",
+    },
+  ],
+  competitions: [],
+
+  // Professional Experience
+  experience: [
+    {
+      title: "CEO",
+      company: "UnderCodes",
+      location: "Colombia",
+      period: "2023 - Present",
+      description:
+        "Lidero UnderCodes, una empresa de desarrollo de software y branding design. Me enfoco en ofrecer soluciones innovadoras a través de la creación de software personalizado y servicios de branding.",
+      technologies: [
+        "Kotlin Multiplatform",
+        ".NET",
+        "Angular",
+        "JavaScript",
+        "TypeScript",
+        "Node.js",
+        "Docker",
+        "Kubernetes",
+      ],
+    },
+    {
+      title: "CTO",
+      company: "Olympus Analytics",
+      location: "Colombia",
+      period: "2023 - Present",
+      description:
+        "Como CTO de Olympus Analytics, lidero el desarrollo de soluciones tecnológicas avanzadas para análisis de datos y optimización de procesos. Mi trabajo se enfoca en el uso de analítica avanzada y el diseño de soluciones de software robustas.",
+      technologies: [
+        "Python",
+        "Angular",
+        "PostgreSql",
+        "Machine Learning",
+        "Data Analytics",
+        "Cloud Computing",
+      ],
+    },
+    {
+      title: "Full Stack Developer & Architect",
+      company: "Krea & Solventech",
+      location: "Colombia",
+      period: "2020 - 2023",
+      description:
+        "Trabajé como arquitecto de software en Krea y Solventech, participando en la creación de soluciones de software personalizadas. Estuve a cargo del diseño y desarrollo de plataformas de gestión, trabajando con tecnologías como Angular, .NET, y más.",
+      technologies: ["Unity", ".NET", "Firebase"],
+      images: [
+        {
+          url: "https://example.com/krea_logo.jpg",
+          alt: "Krea Logo",
+        },
+      ],
+    },
+    {
+      title: "Junior Research Assistant",
+      company: "Universidad del Norte",
+      location: "Barranquilla, Colombia",
+      period: "Summer 2024",
+      description:
+        "Asistí en el desarrollo de un proyecto de realidad virtual (VR) utilizando Unity. Colaboré en la creación de entornos virtuales interactivos y en la implementación de características para mejorar la experiencia del usuario.",
+      technologies: ["Unity", "C#", "VR", "3D Modeling", "User Experience"],
+    },
+    {
+      title: "Presidente & Director Creativo",
+      company: "Innova (Grupo Estudiantil)",
+      location: "Universidad del Norte",
+      period: "2022 - 2023",
+      description:
+        "Fui presidente y director creativo del grupo estudiantil Innova, donde diseñé la identidad visual del grupo y lideré proyectos orientados a la innovación tecnológica y el desarrollo personal de los estudiantes.",
+      technologies: ["Diseño Gráfico", "Branding", "Marketing Digital"],
+      images: [
+        {
+          url: "https://example.com/innova_logo.jpg",
+          alt: "Innova Logo",
+        },
+      ],
+    },
+  ],
+
+  // SEO Configuration
+  seo: {
+    title: "Cristhian Agamez",
+    description:
+      "Software Developer and Software Architect specializing in creating robust's and efficient softwares",
+    keywords: [
+      "Software Developer",
+      "Software Architect",
+      "Angular Dev",
+      "Frontend Dev",
+      "Backend Dev",
+      "Astro Dev",
+      "React Dev",
+      "Go",
+      "Django",
+      ".Net",
+    ],
+  },
+
+  // Theme Configuration
+  theme: {
+    primaryColor: "#1ED760", // Spotify green
+    secondaryColor: "#1d1d1f",
+    accentColor: "#007AFF",
+  },
+
+  // Projects Configuration
+  projects: [
+    scheds,
+    portfolio,
+    foodies,
+    fadedTextRestoration,
+    nucpaBalloons,
+    nucpaBalloonsApi,
+    neurospeccompanionmerged,
+    clinicalmain,
+    seatReservation,
+
+    // Add more projects here
+  ],
+} as const;
